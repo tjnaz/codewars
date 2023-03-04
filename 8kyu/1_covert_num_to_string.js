4b8553fa1e2885831f5a047869f9f9aae386f51d
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-04 11:13:52 +0530

    8kyu: number to string conversion

---
+++ 8kyu/1_covert_num_to_string.js
@@ -0,0 +1,16 @@
+// Description:
+
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+// Examples (input --> output):
+
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+function convertNumToString(num) {
+  return String(num);
+}
+
+// console.log(convertNumToString(10));
