b9cecf0abea7696fb95328f755345df5b1985f68
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-06 15:31:01 +0530

    8kyu: Total Points - Good one

---
+++ 8kyu/9_total_points.js
@@ -0,0 +1,53 @@
+// --- ************ --- //
+// --- [2023-03-06] --- //
+// --- ************ --- //
+
+// Total amount of points
+
+// Our football team has finished the championship.
+
+// Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+//     if x > y: 3 points (win)
+//     if x < y: 0 points (loss)
+//     if x = y: 1 point (tie)
+
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+// Notes:
+
+//     our team always plays 10 matches in the championship
+//     0 <= x <= 4
+//     0 <= y <= 4
+
+function points(games) {
+  // First attempt solution. Simple and straight forward
+  let points = 0;
+  games.forEach((e) => {
+    if (e[0] > e[2]) {
+      points += 3;
+    } else if (e[0] == e[2]) {
+      points += 1;
+    }
+  });
+  return points;
+}
+
+console.log(
+  points([
+    "1:0",
+    "2:0",
+    "3:0",
+    "4:0",
+    "2:1",
+    "1:3",
+    "1:4",
+    "2:3",
+    "2:4",
+    "3:4",
+  ]),
+);
