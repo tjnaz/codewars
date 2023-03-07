1ade5b135ce3fae5b41abc529c0dbe4d3cb47d52
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-07 18:53:04 +0530

    8kyu: even or odd

---
+++ 8kyu/10_even_or_odd.js
@@ -0,0 +1,15 @@
+// --- ************ --- //
+// --- [2023-03-07] --- //
+// --- ************ --- //
+
+// Even or Odd
+
+// Description:
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(num) {
+  return num % 2 === 0 ? "Even" : "Odd";
+}
+
+console.log(evenOrOdd(6));
