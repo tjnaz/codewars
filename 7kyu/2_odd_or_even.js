00385b4dfe863c6e2de00fbc85274c55655b45ab
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-08 13:50:32 +0530

    7kyu: odd or even for the sum of an array

---
+++ 7kyu/2_odd_or_even.js
@@ -0,0 +1,34 @@
+// --- ************ --- //
+// --- [2023-03-08] --- //
+// --- ************ --- //
+
+// Odd or Even?
+
+// Description:
+// Task:
+
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+// Examples:
+
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+
+// Have fun!
+
+function oddOrEven(arr) {
+  return arr.reduce((acc, item) => (acc += item), 0) % 2 ? "odd" : "even";
+}
+
+let arr = [0, 1, 4];
+
+console.log(oddOrEven(arr));
