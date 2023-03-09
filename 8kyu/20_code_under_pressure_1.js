b9bcdfe81c53b8e9e6137ec06b69e1a32cd0cd13
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-09 18:59:37 +0530

    8kyu: code under pressure

---
+++ 8kyu/20_code_under_pressure_1.js
@@ -0,0 +1,13 @@
+// --- ************ --- //
+// --- [2023-03-09] --- //
+// --- ************ --- //
+
+// You Can't Code Under Pressure #1
+
+// Code as fast as you can! You need to double the integer and return it.
+
+function double(num) {
+  return num * 2;
+}
+
+console.log(double(5));
