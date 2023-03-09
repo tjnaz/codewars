2b4e98442a068c5b090e4aa2025f0f82c56b18b1
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-09 17:21:12 +0530

    8kyu: rps

---
+++ 8kyu/17_rock_paper_scissors.js
@@ -0,0 +1,34 @@
+// --- ************ --- //
+// --- [2023-03-09] --- //
+// --- ************ --- //
+
+// Rock Paper Scissors!
+
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+function rockPaperScissors(p1, p2) {
+  p1.toLowerCase();
+  p2.toLowerCase();
+
+  if (p1 == p2) {
+    return "Draw!";
+  }
+
+  if (
+    (p1 == "scissors" && p2 == "paper") ||
+    (p1 == "paper" && p2 == "rock") ||
+    (p1 == "rock" && p2 == "scissors")
+  ) {
+    return "Player 1 won!";
+  } else {
+    return "Player 2 won!";
+  }
+}
+
+console.log(rockPaperScissors("paper", "scissors"));
