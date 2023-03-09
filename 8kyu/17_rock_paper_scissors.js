2aafa0c1b5ed19ad7f4e30c2aab22924222d7350
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-09 17:24:33 +0530

    Add a clever solution from codewars

--- 8kyu/17_rock_paper_scissors.js
+++ 8kyu/17_rock_paper_scissors.js
@@ -16,19 +16,28 @@ function rockPaperScissors(p1, p2) {
   p1.toLowerCase();
   p2.toLowerCase();
 
-  if (p1 == p2) {
-    return "Draw!";
-  }
+  // MY SOLUTION <----
+  // if (p1 == p2) {
+  //   return "Draw!";
+  // }
 
-  if (
-    (p1 == "scissors" && p2 == "paper") ||
-    (p1 == "paper" && p2 == "rock") ||
-    (p1 == "rock" && p2 == "scissors")
-  ) {
+  // if (
+  //   (p1 == "scissors" && p2 == "paper") ||
+  //   (p1 == "paper" && p2 == "rock") ||
+  //   (p1 == "rock" && p2 == "scissors")
+  // ) {
+  //   return "Player 1 won!";
+  // } else {
+  //   return "Player 2 won!";
+  // }
+
+  // AWESOME SOLUTION FROM CODEWAR
+  if (p1 === p2) return "Draw!";
+  let rules = { rock: "scissors", paper: "rock", scissors: "paper" };
+  if (p2 === rules[p1]) {
     return "Player 1 won!";
   } else {
     return "Player 2 won!";
   }
 }
-
 console.log(rockPaperScissors("paper", "scissors"));
