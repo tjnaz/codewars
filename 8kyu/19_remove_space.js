127916403f4c95fa15c6edeb7c742fe1f6fc4176
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-09 18:36:51 +0530

    8kyu: remove spaces from string

---
+++ 8kyu/19_remove_space.js
@@ -0,0 +1,15 @@
+// --- ************ --- //
+// --- [2023-03-09] --- //
+// --- ************ --- //
+
+// Remove String Spaces
+
+// Description:
+
+// Simple, remove the spaces from the string, then return the resultant string.
+
+function removeSpace(str) {
+  return str.split(" ").join("");
+}
+
+console.log(removeSpace("E !tt nth!"));
