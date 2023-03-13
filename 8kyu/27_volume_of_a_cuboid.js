69b773db414460e88c41529342b67960c00bccbb
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-13 17:59:13 +0530

    8kyu: volume of a cuboid

---
+++ 8kyu/27_volume_of_a_cuboid.js
@@ -0,0 +1,14 @@
+// --- ************ --- //
+// --- [2023-03-13] --- //
+// --- ************ --- //
+
+// Volume of a Cuboid
+// Description:
+
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+function volumeOfACuboid(l, w, h) {
+  return l * w * h;
+}
+
+console.log(volumeOfACuboid(3, 5, 3));
