4b2e69602025d00478e16b671f4a9e9526d94fac
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-15 12:44:51 +0530

    8kyu: make uppercase

---
+++ 8kyu/28_make_upperCase.js
@@ -0,0 +1,14 @@
+// --- ************ --- //
+// --- [2023-03-15] --- //
+// --- ************ --- //
+
+// MakeUpperCase
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+
+const str = "";
+
+console.log(makeUpperCase(str));
