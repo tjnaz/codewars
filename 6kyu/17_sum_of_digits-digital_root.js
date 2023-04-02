51ca50045259e0107c2e01003987f27a0a918217
Thaj Anaz <anaz.thaj@gmail.com>
2023-04-02 23:24:15 +0530

    complete: 6kyu - sum of digits (ugghh recursive!!)

---
+++ 6kyu/17_sum_of_digits-digital_root.js
@@ -0,0 +1,26 @@
+// -- ************ --- //
+// --- [2023-04-02] --- //
+// --- ************ --- //
+
+// Sum of Digits / Digital Root
+// Description:
+
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+// Examples
+
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function findDigitalRoot(num) {
+  const rootTotal = num
+    .toString()
+    .split("")
+    .reduce((acc, e) => (acc += +e), 0);
+  return rootTotal < 10 ? rootTotal : findDigitalRoot(rootTotal);
+}
+
+console.log(findDigitalRoot(51395131));
