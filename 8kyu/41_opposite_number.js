4469a9929336e8a69b02e9bc0de434f8f69f5b9e
Thaj Anaz <anaz.thaj@gmail.com>
2023-04-05 22:39:42 +0530

    complete: 8kyu - opposite number

---
+++ 8kyu/41_opposite_number.js
@@ -0,0 +1,20 @@
+// -- ************ --- //
+// --- [2023-04-05] --- //
+// --- ************ --- //
+
+// Opposite number
+// Description:
+
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function negateNumber(num) {
+  return num * -1;
+}
+
+console.log(negateNumber(-5));
